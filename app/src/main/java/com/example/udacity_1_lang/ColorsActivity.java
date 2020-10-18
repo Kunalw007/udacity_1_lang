584b2a40a4b733336592da4ee1b6aec8b2387dce
Kunal Wadhwani <kunalwadhwani7@gmail.com>
2020-10-18 21:21:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/udacity_1_lang/ColorsActivity.java
@@ -0,0 +1,14 @@
+package com.example.udacity_1_lang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ColorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colors);
+    }
+}
